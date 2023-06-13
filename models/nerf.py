42d778d3ac634752648c3a3d61867ac06c3e3c59
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-06-13 15:56:06 +0800

    update the config and physics nerf

--- models/nerf.py
+++ models/nerf.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
 
-class VanillaNERF(nn.Module):
+class VanillaNerf(nn.Module):
     def __init__(self, config):
         super().__init__()
 
     def forward(self, x, c2w):return x
+
