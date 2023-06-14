005a4a383db51d13ba18e00b8793ef1ab015e7f7
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-06-14 21:47:29 +0800

    add some configuration

--- config.py
+++ config.py
@@ -6,4 +6,8 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 parser = argparse.ArgumentParser()
 parser.add_argument("--device",                         default = device)
 parser.add_argument("--resolution",                     default = (128,128))
+
+# grid-evolution configuration
+parser.add_argument("--voxel_size",                     default = (64,64,64))
+parser.add_argument("--points_per_voxel",               default = 5)
 config = parser.parse_args(args = [])
--- models/grid.py
+++ models/grid.py
@@ -2,3 +2,13 @@ import torch
 import torch.nn as nn
 
 import numpy as np
+
+class GridDynamics(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.voxel_size = config.voxel_size
+        self.points_per_voxel = config.points_per_voxel
+
+    def state_evolve(self, state):
+        return state
+        
