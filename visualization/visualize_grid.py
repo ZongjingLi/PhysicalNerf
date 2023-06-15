de63031003f37e79eca256d68e22a057a1d86ead
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-06-15 11:01:27 +0800

    update the visualization of the grid.

--- visualization/visualize_grid.py
+++ visualization/visualize_grid.py
@@ -8,21 +8,35 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+def visualize_grid(voxel, file_name = "outputs/voxel.png"):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    # create the 3d canvas and setup the normalized coorindate
+    rang = 1.0
+    ax.set_xlim(-rang, rang)
+    ax.set_ylim(-rang, rang)
+    ax.set_zlim(-rang, rang)
+
+    # plot the voxel grid.
+
+    return 0
 
 def visualize_points(x_points, file_name = "outputs/points.png"):
     fig = plt.figure()
     ax = Axes3D(fig)
-    # create the 3d canvas and setup the normalize coordinate
+    # create the 3d canvas and setup the normalized coordinate
     rang = 1.0
-    ax.set_zlim(-rang,rang)
-    ax.set_xlim(-rang,rang)
-    ax.set_ylim(-rang,rang)
+    ax.set_xlim(-rang, rang)
+    ax.set_ylim(-rang, rang)
+    ax.set_zlim(-rang, rang)
 
     # scatter points on the axis
     ax.scatter(x_points[:,0], x_points[:,1], x_points[:,2], color = "cyan")
     plt.savefig(file_name)
 
 if __name__ == "__main__":
+    size = 64
     points = torch.randn([100, 3]) * 0.2
-
+    voxel_grid = torch = torch.randn([size, size, size, 3])
     visualize_points(points)
+    visualize_grid(voxel_grid)
