68481ef4dbaee28764e71f761494fc54903365ba
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-06-15 18:07:27 +0800

    delete the previous part of visualization

--- visualization/visualize_grid.py
+++ visualization/visualize_grid.py
@@ -34,6 +34,23 @@ def visualize_points(x_points, file_name = "outputs/points.png"):
     ax.scatter(x_points[:,0], x_points[:,1], x_points[:,2], color = "cyan")
     plt.savefig(file_name)
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torchvision
+from matplotlib.patches import Rectangle
+
+def visualize_image_grid(images, row, save_name = "image_grid"):
+    plt.figure(save_name, frameon = False);plt.cla()
+    plt.tick_params(left = False, right = False , labelleft = False ,
+                labelbottom = False, bottom = False)
+    images = images.permute([0,3,1,2])
+    comps_grid = torchvision.utils.make_grid(images,normalize=True,nrow=row).permute([1,2,0])
+
+    plt.imshow(comps_grid.cpu().detach().numpy())
+    plt.savefig("outputs/{}.png".format(save_name), bbox_inches='tight', pad_inches=0)
+
 if __name__ == "__main__":
     size = 64
     points = torch.randn([100, 3]) * 0.2
