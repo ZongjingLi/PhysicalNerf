bb6eb1456c97bbd8a04543cfaeef05338e9e4f0f
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-07-03 00:18:34 +0800

    update

--- debug.py
+++ debug.py
@@ -32,43 +32,34 @@ def render():
             pixels[i, j] = setcolor(z, count)
 
 render()
-
-from PIL import Image
-import IPython
-import matplotlib.pyplot as plt
-
-img = Image.fromarray((255 * pixels.to_numpy()).astype('uint8')).transpose(Image.Transpose.TRANSPOSE)
-#IPython.display.display(img)
-
-plt.imshow(img)
-#plt.show()
-
-from scene import Scene
 import taichi as ti
-from taichi.math import *
+import taichi.math as tm
 
-scene = Scene(exposure=10)
-scene.set_floor(-0.05, (1.0, 1.0, 1.0))
+ti.init(arch=ti.gpu)
 
+n = 320
+pixels = ti.field(dtype=float, shape=(n * 2, n))
+
+@ti.func
+def complex_sqr(z):  # complex square of a 2D vector
+    return tm.vec2(z[0] * z[0] - z[1] * z[1], 2 * z[0] * z[1])
 
 @ti.kernel
-def initialize_voxels():
-    n = 50
-    for i, j in ti.ndrange(n, n):
-        if min(i, j) == 0 or max(i, j) == n - 1:
-            scene.set_voxel(vec3(i, 0, j), 2, vec3(0.9, 0.1, 0.1))
-        else:
-            scene.set_voxel(vec3(i, 0, j), 1, vec3(0.9, 0.1, 0.1))
+def paint(t: float):
+    for i, j in pixels:  # Parallelized over all pixels
+        c = tm.vec2(-0.8, tm.cos(t) * 0.2)
+        z = tm.vec2(i / n - 1, j / n - 0.5) * 2
+        iterations = 0
+        while z.norm() < 20 and iterations < 50:
+            z = complex_sqr(z) + c
+            iterations += 1
+        pixels[i, j] = 1 - iterations * 0.02
 
-            if ti.random() < 0.04:
-                height = int(ti.random() * 20)
+gui = ti.GUI("Julia Set", res=(n * 2, n))
 
-                for k in range(1, height):
-                    scene.set_voxel(vec3(i, k, j), 1, vec3(0.0, 0.5, 0.9))
-                if height:
-                    scene.set_voxel(vec3(i, height, j), 2, vec3(1, 1, 1))
-
-
-initialize_voxels()
-
-scene.finish()
+i = 0
+while gui.running:
+    paint(i * 0.03)
+    gui.set_image(pixels)
+    gui.show()
+    i += 1
