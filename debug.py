04cf81ae7481c9dd6d4bf7ddb840ee4d93c4c388
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-08-11 23:13:42 +0800

    confirm the environment

--- debug.py
+++ debug.py
@@ -1,31 +1,31 @@
 import taichi as ti
-import taichi.math as tm
+import taichi_three as t3
+import numpy as np
 
-ti.init(arch = ti.gpu)
+ti.init(ti.cpu)
 
-n = 320
-pixels = ti.field(dtype = float, shape = (2*n, n))
+scene = t3.Scene()
+model = t3.Model(t3.Mesh.from_obj(t3.readobj('assets/torus.obj', scale=0.8)))
+model.material = t3.Material(t3.CookTorrance(
+    color=t3.Texture(ti.imread('assets/cloth.jpg')),
+    roughness=t3.Texture(ti.imread('assets/pattern.jpg')),
+    metallic=t3.Constant(0.5),
+    ))
+scene.add_model(model)
+camera = t3.Camera()
+camera.ctl = t3.CameraCtl(pos=[0.8, 0, 2.5])
+scene.add_camera(camera)
+light = t3.Light([0, -0.5, -1])
+scene.add_light(light)
+ambient = t3.AmbientLight(0.3)
+scene.add_light(ambient)
 
-@ti.func
-def complex_sqr(z):
-    return tm.vec2(z[0]*z[0] - z[1]*z[1], 2*z[0]*z[1])
-
-@ti.kernel
-def paint(t: float):
-    for x,y in pixels:
-        c = tm.vec2(-.8, tm.cos(t) * .2)
-        z = tm.vec2(x / n - 1, y / n - 0.5) * 2
-        iterations = 0
-        while z.norm() < 20 and iterations < 50:
-            iterations += 1
-            z = complex_sqr(z) + c
-        pixels[x,y] = 1 - iterations * 0.02
-
-gui = ti.GUI("test_run", res = (2*n, n))
-
-t = 0
+gui = ti.GUI('Model', camera.res)
 while gui.running:
-    paint(t * 0.03)
-    gui.set_image(pixels)
+    gui.get_event(None)
+    gui.running = not gui.is_pressed(ti.GUI.ESCAPE)
+    camera.from_mouse(gui)
+    model.L2W[None] = t3.rotateX(angle=t3.get_time())
+    scene.render()
+    gui.set_image(camera.img)
     gui.show()
-    t += 1
